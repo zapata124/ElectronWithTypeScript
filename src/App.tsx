8a59fd915b6e4bf1a766934d3e9d8cb4a7e19eb9
Eddy Zapata <ecz001@live.com>
2023-01-11 19:40:45 -0500

    Adding AppLayout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Drawer, SideBar, Header, Footer } from './components';
+import AppLayout from './components/AppLayout';
 const App = () => {
   return (
-    <>
-      <Header
-        user={{
-          name: '',
-          image: '',
-        }}
-        tabs={[]}
-      />
-      <SideBar />
-      <Footer />
-    </>
+    // <>
+    //   <Header
+    //     user={{
+    //       name: '',
+    //       image: '',
+    //     }}
+    //     tabs={[]}
+    //   />
+    //   <SideBar />
+    //   <Footer />
+    // </>
+    <AppLayout />
   );
 };
 
