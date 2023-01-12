cafdef823b2193a9e3e6e95c8a9ee74cfaa9ac65
Eddy Zapata <ecz001@live.com>
2023-01-11 20:02:46 -0500

    new applayout grid

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,27 +1,64 @@
-import { AppShell, Navbar, Header } from '@mantine/core';
+import { useState } from 'react';
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from '@mantine/core';
 
 const AppLayout = () => {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
   return (
     <AppShell
-      padding='md'
+      styles={{
+        main: {
+          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint='sm'
+      asideOffsetBreakpoint='sm'
       navbar={
-        <Navbar width={{ base: 300 }} height={500} p='xs'>
-          {/* Navbar content */}
+        <Navbar p='md' hiddenBreakpoint='sm' hidden={!opened} width={{ sm: 200, lg: 300 }}>
+          <Text>Application navbar</Text>
         </Navbar>
       }
+      aside={
+        <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+          <Aside p='md' hiddenBreakpoint='sm' width={{ sm: 200, lg: 300 }}>
+            <Text>Application sidebar</Text>
+          </Aside>
+        </MediaQuery>
+      }
+      footer={
+        <Footer height={60} p='md'>
+          Application footer
+        </Footer>
+      }
       header={
-        <Header height={60} p='xs'>
-          {/* Header content */}
+        <Header height={{ base: 50, md: 70 }} p='md'>
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size='sm'
+                color={theme.colors.gray[6]}
+                mr='xl'
+              />
+            </MediaQuery>
+
+            <Text>Application header</Text>
+          </div>
         </Header>
       }
-      styles={(theme) => ({
-        main: {
-          backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-        },
-      })}
     >
-      {/* Your application here */}
+      <Text>Resize app to see responsive navbar in action</Text>
     </AppShell>
   );
 };
