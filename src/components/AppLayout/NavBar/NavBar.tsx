e1ba6ef725d501eb056b71de4cbb5e0546d21f63
Eddy Zapata <ecz001@live.com>
2023-01-12 23:32:16 -0500

    adding sidebar icon menus

--- src/components/AppLayout/NavBar/NavBar.tsx
+++ src/components/AppLayout/NavBar/NavBar.tsx
@@ -1,11 +1,39 @@
 import { ReactElement } from 'react';
-import { Navbar, Text } from '@mantine/core';
+import { Navbar, Text, Tooltip, UnstyledButton } from '@mantine/core';
+import {
+  IconHome2,
+  IconGauge,
+  IconDeviceDesktopAnalytics,
+  IconFingerprint,
+  IconCalendarStats,
+  IconUser,
+  IconSettings,
+} from '@tabler/icons';
 interface NavBarType {
-  opened: boolean 
+  opened: boolean;
 }
+const mainLinksMockdata = [
+  { icon: IconHome2, label: 'Home' },
+  { icon: IconGauge, label: 'Dashboard' },
+  { icon: IconDeviceDesktopAnalytics, label: 'Analytics' },
+  { icon: IconCalendarStats, label: 'Releases' },
+  { icon: IconUser, label: 'Account' },
+  { icon: IconFingerprint, label: 'Security' },
+  { icon: IconSettings, label: 'Settings' },
+];
+const mainLinks = mainLinksMockdata.map((link) => (
+  <Tooltip label={link.label} position='right' withArrow transitionDuration={0} key={link.label}>
+    <UnstyledButton
+      // onClick={() => setActive(link.label)}
+      // className={cx(classes.mainLink, { [classes.mainLinkActive]: link.label === active })}
+    >
+      <link.icon stroke={1.5} />
+    </UnstyledButton>
+  </Tooltip>
+));
 const NavBar: React.FC<NavBarType> = ({ opened }: NavBarType): ReactElement => {
   return (
-    <Navbar  hidden={opened} width={{ sm: opened? 200 : 65, lg: opened? 300 : 65 }}>
+    <Navbar hidden={opened} width={{ sm: opened ? 200 : 65, lg: opened ? 300 : 65 }}>
       {/* <Text>Application navbar</Text> */}
     </Navbar>
   );
