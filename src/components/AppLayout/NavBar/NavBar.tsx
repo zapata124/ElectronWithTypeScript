d85bcd1a1637f89005bed47c2f34627ccb0cb2cb
Eddy Zapata <ecz001@live.com>
2023-01-13 18:23:28 -0500

    adding padding inside bar

--- src/components/AppLayout/NavBar/NavBar.tsx
+++ src/components/AppLayout/NavBar/NavBar.tsx
@@ -21,33 +21,56 @@ const sideBarIcons = [
   { icon: IconFingerprint, label: 'Security' },
   { icon: IconSettings, label: 'Settings' },
 ];
-const NavBarClosedGrid = ({ children, opened }: any) => {
+const NavBarOpenedGrid = ({ children }: any) => {
   return (
-    <Grid.Col xs={opened ? 2 : 12} sx={{ backgroundColor: 'blue' }}>
+    <Grid.Col xs={10} >
       {children}
     </Grid.Col>
   );
 };
-const NavBarOpenedGrid = ({ children }: any) => {
+const NavBarClosedGrid = ({ children, opened }: any) => {
   return (
-    <Grid.Col xs={10} sx={{ backgroundColor: 'yellow' }}>
+    <Grid.Col
+      xs={opened ? 2 : 12}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       {children}
+      {opened && (
+        <NavBarOpenedGrid>
+          <IconHome2 />
+        </NavBarOpenedGrid>
+      )}
     </Grid.Col>
   );
 };
 const SideBarIcons = ({ sideMenuIcons }: any) => {
-  return sideMenuIcons.map((icon: any) => (
-    <Tooltip label={icon.label} position='right' withArrow transitionDuration={0} key={icon.label}>
-      <UnstyledButton
-      // onClick={() => setActive(link.label)}
-      // className={cx(classes.mainLink, { [classes.mainLinkActive]: link.label === active })}
-      >
-        <icon.icon stroke={1.5} />
-      </UnstyledButton>
-    </Tooltip>
-  ));
+  return (
+    <Grid sx={{ paddingTop: 20 }}>
+      {sideMenuIcons.map((icon: any, index: number) => (
+        <NavBarClosedGrid key={`nav-${index}`}>
+          <Tooltip
+            label={icon.label}
+            position='right'
+            withArrow
+            transitionDuration={0}
+            key={icon.label}
+          >
+            <UnstyledButton
+            // onClick={() => setActive(link.label)}
+            // className={cx(classes.mainLink, { [classes.mainLinkActive]: link.label === active })}
+            >
+              <icon.icon stroke={1.5} />
+            </UnstyledButton>
+          </Tooltip>
+        </NavBarClosedGrid>
+      ))}
+    </Grid>
+  );
 };
-
 const NavBar: React.FC<NavBarType> = ({ opened }: NavBarType): ReactElement => {
   return (
     <Navbar hidden={opened} width={{ sm: opened ? 200 : 65, lg: opened ? 300 : 65 }}>
