e60c62ca22b64fefe65f940d828531b95f47e6f7
Eddy Zapata <ecz001@live.com>
2023-01-20 22:45:32 -0500

    Update NavBar.tsx

--- src/components/AppLayout/NavBar/NavBar.tsx
+++ src/components/AppLayout/NavBar/NavBar.tsx
@@ -19,6 +19,20 @@ const sideBarIcons = [
   { icon: IconUser, label: 'Account' },
   { icon: IconFingerprint, label: 'Security' },
   { icon: IconSettings, label: 'Settings' },
+  { icon: IconHome2, label: 'Home' },
+  { icon: IconGauge, label: 'Dashboard' },
+  { icon: IconDeviceDesktopAnalytics, label: 'Analytics' },
+  { icon: IconCalendarStats, label: 'Releases' },
+  { icon: IconUser, label: 'Account' },
+  { icon: IconFingerprint, label: 'Security' },
+  { icon: IconSettings, label: 'Settings' },
+  { icon: IconHome2, label: 'Home' },
+  { icon: IconGauge, label: 'Dashboard' },
+  { icon: IconDeviceDesktopAnalytics, label: 'Analytics' },
+  { icon: IconCalendarStats, label: 'Releases' },
+  { icon: IconUser, label: 'Account' },
+  { icon: IconFingerprint, label: 'Security' },
+  { icon: IconSettings, label: 'Settings' },
 ];
 
 const NavBarOpenedGrid = () => {
