cec80f7e4ac2c867c835f92e302ac05eca8a8502
Eddy Zapata <ecz001@live.com>
2023-01-21 19:24:22 -0500

    trying to add dynamic routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
         element: <Navigate to={'Content'} />,
       },
       {
-        path: 'Content',
+        path: 'Content/',
         element: <Outlet />,
         children: [
           {
